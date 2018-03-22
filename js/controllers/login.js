a180d232906fcbd0b21ef62a5127100bf608fbb2
priyanalleboina <37654989+priyanalleboina@users.noreply.github.com>
2018-03-22 16:02:56 +0530

    Update login.js

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,21 +1,21 @@
 angular.module('myapp').controller("loginctl",function($scope,$location,myservice,$state){
 $scope.dashboard=function(){
-    // $location.path("/dashboard")
-var loginDetails=   {
-        nationalId:$scope.uname,
-        password:$scope.pwd
-    }
+    $location.path("/dashboard")
+// var loginDetails=   {
+//         nationalId:$scope.uname,
+//         password:$scope.pwd
+//     }
    
-    JSON.stringify(loginDetails);
-  myservice.post("http://shamba.shambadigital.com/Shamba/login",loginDetails).then(function success(response){
-        var credential=response;
-        console.log(credential);
-        if(credential.status==="success"){
-            $state.go("dashboard");
-        }
-        else{
-            alert("username or password incorrect");
-        }
-    })
+//     JSON.stringify(loginDetails);
+//   myservice.post("http://shamba.shambadigital.com/Shamba/login",loginDetails).then(function success(response){
+//         var credential=response;
+//         console.log(credential);
+//         if(credential.status==="success"){
+//             $state.go("dashboard");
+//         }
+//         else{
+//             alert("username or password incorrect");
+//         }
+//     })
 }
 })
